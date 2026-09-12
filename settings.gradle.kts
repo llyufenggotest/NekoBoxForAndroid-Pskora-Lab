b8f2990d7ed730e9a38c46ab70d5fd4f49d07bdf
MagicMod <magic@mod.com>
2026-09-13 02:06:23 +0800

    build: add canonical Maven Central endpoint

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,7 @@ pluginManagement {
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven(url = "https://repo.maven.apache.org/maven2")
         maven(url = "https://repo1.maven.org/maven2")
         maven(url = "https://plugins.gradle.org/m2")
     }
