ea199a7a2b5dbbe7e9c0444a2b6e1dcc0c1ef0bb
MagicMod <magic@mod.com>
2026-09-13 02:43:53 +0800

    build: prefer canonical artifact repositories

--- repositories.gradle.kts
+++ repositories.gradle.kts
@@ -1,9 +1,9 @@
 repositories {
-    maven(url = "https://maven.aliyun.com/repository/google")
-    maven(url = "https://maven.aliyun.com/repository/central")
-    maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
     google()
     mavenCentral()
     gradlePluginPortal()
     maven(url = "https://jitpack.io")
+    maven(url = "https://maven.aliyun.com/repository/google")
+    maven(url = "https://maven.aliyun.com/repository/central")
+    maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,14 @@
 pluginManagement {
     repositories {
-        maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
-        maven(url = "https://maven.aliyun.com/repository/google")
-        maven(url = "https://maven.aliyun.com/repository/central")
         gradlePluginPortal()
         google()
         mavenCentral()
         maven(url = "https://repo.maven.apache.org/maven2")
         maven(url = "https://repo1.maven.org/maven2")
         maven(url = "https://plugins.gradle.org/m2")
+        maven(url = "https://maven.aliyun.com/repository/gradle-plugin")
+        maven(url = "https://maven.aliyun.com/repository/google")
+        maven(url = "https://maven.aliyun.com/repository/central")
     }
 }
 
